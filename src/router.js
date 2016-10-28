9f21fb7016d34d20826f5ec5b972c627c24da6bb
杨明明 <ddmingmingyang@gmail.com>
2016-10-28 11:06:25 +0800

    Change router module to router class

--- src/router.js
+++ src/router.js
@@ -1,38 +1,42 @@
 const routeCompiler = require('./lib/route-compiler');
 const urlUtils = require('simple-url');
-
-const regexRouters = [];
-const stringRouters = {};
 var emptyFunc = () => {};
-var onMismatch;
 
-module.exports = {
-  createMismatch: mismatch => {
-    onMismatch = mismatch || emptyFunc;
+function Router(){
+  this.regexRouters = [];
+  this.stringRouters = {};
+  this.onMismatch = emptyFunc;
+}
+
+Router.prototype = {
+  createMismatch: function(mismatch){
+    if(typeof mismatch === 'function'){
+      this.onMismatch = mismatch;
+    }
   },
-  create: (route, callback, strict) => {
+  create: function(route, callback, strict){
     callback = callback || emptyFunc;
     var compiled = routeCompiler.compile(route, strict);
     if(compiled.tokens.length === 0){
-      stringRouters[route] = callback;
+      this.stringRouters[route] = callback;
       if(strict !== true){
-        stringRouters[route.slice(-1) === "/" ? route.slice(0, -1) : route + "/"] = callback;
+        this.stringRouters[route.slice(-1) === "/" ? route.slice(0, -1) : route + "/"] = callback;
       }
     }else{
-      regexRouters.push([compiled, callback]);
+      this.regexRouters.push([compiled, callback]);
     }
   },
-  match: (url, data) => {
+  match: function(url, data){
     var urlObj = urlUtils.parse(url),
       path = urlObj.pathname;
     urlObj.url = url;
-    if(stringRouters[path]){
-      stringRouters[path]({url: urlObj, params: null, data: data});
+    if(this.stringRouters[path]){
+      this.stringRouters[path]({url: urlObj, params: null, data: data});
       return true;
     }else{
-      var i = 0, len = regexRouters.length, router, match;
+      var i = 0, len = this.regexRouters.length, router, match;
       while(i < len){
-        router = regexRouters[i];
+        router = this.regexRouters[i];
         match = router[0].regex.exec(path);
         if(match !== null){
           router[1]({url: urlObj, params: routeCompiler.parsePath(match, router[0].tokens), data: data});
@@ -41,7 +45,9 @@ module.exports = {
         i ++;
       }
     }
-    onMismatch({url: urlObj, data: data});
+    this.onMismatch({url: urlObj, data: data});
     return false;
   }
 };
+
+module.exports = Router;
